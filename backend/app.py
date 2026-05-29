574cb8a8d3a737793809b1fdbaa693121f97e3f7
bg23abh <binayaghimire147@gmail.com>
2026-05-29 11:55:30 +0100

    feat(request-tracker):updating auto formatter

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
-import os
-from datetime import datetime
-from zipfile import BadZipFile
+from flask import Flask, request, jsonify
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from openpyxl import Workbook, load_workbook
+from zipfile import BadZipFile
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 CORS(app)
